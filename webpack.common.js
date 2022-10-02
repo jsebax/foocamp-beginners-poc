de2b2efda3a0fae6b64f9ec51de50068c219bb91
Sebastian Benitez <jsb1439@hotmail.com>
2022-10-01 23:25:04 -0500

    Add prettier and eslint

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,26 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/js/app.js')
+    main: path.resolve(__dirname, './src/js/app.js'),
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'static')
+    path: path.resolve(__dirname, 'static'),
   },
   plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.(scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.js$/,
@@ -23,20 +28,17 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource'
-      }
-    ]
+        type: 'asset/resource',
+      },
+    ],
   },
   optimization: {
-    minimizer: [
-      `...`,
-      new CssMinimizerPlugin()
-    ]
-  }
-};
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+}
